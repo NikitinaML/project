dce197761bbc4b0c81bc238248dae7e243247500
NikitinaML <nikitina.a.e.mail@gmail.com>
2025-01-21 13:04:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -65,20 +65,24 @@ tokenizer = AutoTokenizer.from_pretrained("my_tokenizer")
 
 st.write(f"Для определения темы введите текст")
 user_text = st.text_input('Текст для классификации')
-st.write(len(user_text))
 
-text_pp = preprocess_text(user_text)
-text_token = tokenizer([text_pp], truncation = True, max_length=100, padding='max_length')
+if len(user_text)>0:
     
-for k in text_token.keys():
-    text_token[k] = torch.tensor(text_token[k])
+    text_pp = preprocess_text(user_text)
+    text_token = tokenizer([text_pp], truncation = True, max_length=100, padding='max_length')
         
-    
-    
-# предсказание
-    
-with torch.no_grad():
-    outputs = model_trans(**text_token)
-    logits = outputs.logits
-    label_predict = np.argmax(logits.detach().cpu().numpy(), axis=1)[0]
-    st.write(f"Topic: {idx_to_label[label_predict]}")
+    for k in text_token.keys():
+        text_token[k] = torch.tensor(text_token[k])
+            
+        
+        
+    # предсказание
+        
+    with torch.no_grad():
+        outputs = model_trans(**text_token)
+        logits = outputs.logits
+        label_predict = np.argmax(logits.detach().cpu().numpy(), axis=1)[0]
+        st.write(f"Topic: {idx_to_label[label_predict]}")
+
+else:
+    st.write(f"Вы не ввели текст")
